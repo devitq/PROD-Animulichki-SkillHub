0fa37bf7fc665b6dab3589cb90ab271cdd439f9b
ITQ <itq.dev@ya.ru>
2024-04-01 14:06:30 +0300

    [chore] Improvements in docker structure

--- backend/project/api/urls.py
+++ backend/project/api/urls.py
@@ -1,6 +1,31 @@
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(title="SkillHub API", default_version="v1"),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path("ping", include("api.ping.urls")),
     path("auth", include("api.users.urls")),
+    # API documentation
+    path(
+        "swagger<format>/",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ]
--- backend/project/config/urls.py
+++ backend/project/config/urls.py
@@ -1,16 +1,6 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
-
-schema_view = get_schema_view(
-    openapi.Info(title="SkillHub API", default_version="v1"),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
 
 urlpatterns = [
     # Built-in urls
@@ -21,22 +11,6 @@ urlpatterns = [
             "rest_framework.urls",
             namespace="rest_framework",
         ),
-    ),
-    # API documentation
-    path(
-        "swagger<format>/",
-        schema_view.without_ui(cache_timeout=0),
-        name="schema-json",
-    ),
-    path(
-        "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    path(
-        "redoc/",
-        schema_view.with_ui("redoc", cache_timeout=0),
-        name="schema-redoc",
     ),
     # API
     path("api/", include("api.urls")),
