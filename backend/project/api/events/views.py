1764490d74757edec62b655a4bbf1e13eee7b0d6
ITQ <itq.dev@ya.ru>
2024-04-03 01:30:59 +0300

    Added event users endpoint

--- backend/project/api/events/views.py
+++ backend/project/api/events/views.py
@@ -1,9 +1,29 @@
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 
 from api.events.models import Event
 from api.events.serializers import EventSerializer
+from api.users.serializers import UserSerializer
 
 
 class EventViewSet(ModelViewSet):
     queryset = Event.objects.all()
     serializer_class = EventSerializer
+
+
+class EventUsersApiView(APIView):
+    def get(self, request, event_id):  # noqa: ARG002
+        try:
+            event = Event.objects.get(pk=event_id)
+        except Event.DoesNotExist:
+            return Response(
+                {"error": "Event does not exist"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        users = event.users.all()
+        serializer = UserSerializer(users, many=True)
+
+        return Response(serializer.data)
