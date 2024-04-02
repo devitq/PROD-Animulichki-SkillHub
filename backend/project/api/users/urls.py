635dfcd114532315f5012e40608672d2dec8a188
Тимур <ochilov-timka@yandex.ru>
2024-04-03 02:40:41 +0300

    [feat] users by event

--- backend/project/api/users/urls.py
+++ backend/project/api/users/urls.py
@@ -5,6 +5,7 @@ from api.users.views import (
     DownloadUsersFromExcelView,
     RegisterUsersFromExcelView,
     UserViewSet,
+    UsersByEvent,
 )
 
 app_name = "users"
@@ -16,6 +17,11 @@ router.register("", UserViewSet)
 urlpatterns = [
     path("", include(router.urls)),
     path(
+        "by-event/<event_id>/",
+        UsersByEvent.as_view(),
+        name="users-by-event",
+    ),
+    path(
         "upload/excel/<event_id>/",
         RegisterUsersFromExcelView.as_view(),
         name="excel-upload",
@@ -24,5 +30,5 @@ urlpatterns = [
         "download/excel/<event_id>/",
         DownloadUsersFromExcelView.as_view(),
         name="excel-download",
-    )
+    ),
 ]
