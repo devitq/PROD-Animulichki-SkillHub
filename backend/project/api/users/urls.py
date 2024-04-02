92d2288bcc876f1124b91b9163430396534e46bb
Data-Name-ID <data.name.id@yandex.ru>
2024-04-02 11:19:18 +0300

    [fix] connect router

--- backend/project/api/users/urls.py
+++ backend/project/api/users/urls.py
@@ -1,13 +1,20 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
 
 from api.users.views import (
     RegisterUsersFromExcelView,
     RegisterUserView,
+    UserViewSet,
 )
 
 app_name = "users"
 
+router = routers.DefaultRouter()
+router.register(r"users", UserViewSet)
+
+
 urlpatterns = [
+    path("", include(router.urls)),
     path("register/", RegisterUserView.as_view(), name="register"),
     path(
         "upload/excel/<event_id>/",
