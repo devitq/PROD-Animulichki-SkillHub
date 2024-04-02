4ab1eeeb26328c20ec8124b3f6f435ff8be7cde6
Data-Name-ID <data.name.id@yandex.ru>
2024-04-02 12:19:40 +0300

    [fix] docs urls

--- backend/project/api/urls.py
+++ backend/project/api/urls.py
@@ -1,6 +1,26 @@
+from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(title="SkillHub API", default_version="1")
+)
 
 urlpatterns = [
+    # Built-in views
+    path("admin/", admin.site.urls),
+    # API documentation
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger"),
+        name="swagger",
+    ),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc"),
+        name="redoc",
+    ),
     path(
         "users/",
         include("api.users.urls", namespace="users"),
@@ -12,5 +32,5 @@ urlpatterns = [
     path(
         "trees/",
         include("api.tree.urls", namespace="trees"),
-    )
+    ),
 ]
