a54df648c3e6ac795de5ecbcfa8f51e4b21e39ae
Тимур <ochilov-timka@yandex.ru>
2024-04-02 14:01:35 +0300

    Completed Events, now the user will know more detailed information

--- backend/project/api/events/models.py
+++ backend/project/api/events/models.py
@@ -4,11 +4,51 @@ from api.core.models import BaseModel
 
 
 class Event(BaseModel):
-    title = models.CharField(max_length=255)
+    LIMITATIONS = (
+        ("Everyone", "everyone"),
+        ("School", "school"),
+        ("Student", "student"),
+        ("Professional", "professional"),
+    )
+
+    title = models.CharField(
+        max_length=255,
+    )
+
     users = models.ManyToManyField(
         "users.User",
         related_name="events",
         blank=True,
+    )
+
+    start_date = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    end_date = models.DateField(
+        null=True,
+        blank=True,
+    )
+
+    description = models.TextField(
+        default="",
+    )
+
+    is_online = models.BooleanField(
+        default=True,
+        verbose_name="оффлайн или онлайн",
+    )
+
+    location = models.CharField(
+        max_length=512,
+        default="",
+    )
+
+    limitation = models.CharField(
+        max_length=64,
+        choices=LIMITATIONS,
+        default="everyone",
     )
 
     def __str__(self):
